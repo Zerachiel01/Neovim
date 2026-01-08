5663248ca638ac8b98c7f38c6f2156f8941ec203
Andreas Kaps <kaps@fastec.de>
2026-01-08 12:01:44 +0100

    Fix errors in plugins

--- init.lua
+++ init.lua
@@ -856,7 +856,7 @@ require('lazy').setup({
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
         },
       },
 
@@ -869,7 +869,7 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = {},
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
