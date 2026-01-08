917a11d3d007909dca7d7f028abf86d974c2716e
Andreas Kaps <kaps@fastec.de>
2026-01-08 13:17:15 +0100

    Set ctrl+s to save a file/buffer

--- init.lua
+++ init.lua
@@ -214,6 +214,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Control-S to save a buffer
+vim.keymap.set('', '<C-s>', ':w<CR>', { desc = '[S]ave the current buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
